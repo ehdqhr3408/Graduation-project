5d55fa6311a23948e4601f515a60e51f482050f3
Co9514 <khj950104@naver.com>
2020-05-22 12:02:32 +0900

    API push repository

---
+++ src/main/java/capstone/foodscouter/FoodscouterApplication.java
@@ -0,0 +1,13 @@
+package capstone.foodscouter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodscouterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodscouterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/capstone/foodscouter/FoodscouterApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.foodscouter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodscouterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
